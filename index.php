c16df4c5574586a2bb7527ab60f0b20014271ed6
nadim <nadimhasannayan77@gmail.com>
2024-09-18 19:28:02 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$one = 100;
+$two = 200;
+$three = 300;
+$sum = $one+$two+$three;
+echo "sum is = ".$sum."<br>";
+
+$my = "My name is Nadim Hasan. This is the first projecct in php";
+echo $my;
+
+?>
