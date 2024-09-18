40c41896dc6e602977e6f45efb19915193f20c54
nadim <nadimhasannayan77@gmail.com>
2024-09-18 19:55:16 +0600

    second

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php
-$one = 100;
-$two = 200;
-$three = 300;
-$sum = $one+$two+$three;
-echo "sum is = ".$sum."<br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $one = 100;
+    $two = 200;
+    $result = $one+$two;
+    echo "result is =".$result;
+    $name = "<br>Nadim hasan nayan";
+    echo $name;
 
-$my = "My name is Nadim Hasan. This is the first projecct in php";
-echo $my;
-
-?>
+    ?>
+    
+</body>
+</html>
